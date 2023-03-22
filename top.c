6303bb3d3906b7c578a963606bf08378a974f0a4
Wu Mianzhi <1427207878@qq.com>
2023-03-22 10:47:38 +0800

    add meminfo

--- top.c
+++ top.c
@@ -28,14 +28,40 @@ static void process_info(char* pid, struct proc * p)
 
 void top()
 {
+    FILE* meminfo = fopen("/proc/meminfo", "r");
+    FILE* kinfo = fopen("/proc/kinfo", "r");
     DIR * proc = opendir("/proc");
     struct dirent * dir;
-    struct proc process[PAGE_MAX];
+    struct proc process[PROC_MAX];
     unsigned long cpu_ticks = 0;
     int i = 0;
     printf("TOP START\n");
+
+    /* Read memeory info */
+    unsigned long memory;
+    unsigned long freemem;
+    unsigned long cachedmem;
+    unsigned long buf[5]; // read meminfo buffer
+    fscanf(meminfo, "%lu %lu %lu %lu %lu", &(buf[0]), &(buf[1]), &(buf[2]), &(buf[3]), &(buf[4]));
+    memory = buf[0] * buf[1] / 1024;
+    freemem = buf[0] * buf[2] / 1024;
+    cachedmem = buf[0] * buf[4] / 1024;
+    printf("main memory: %luK total, %luK free, %luK cached\n", memory, freemem, cachedmem);
+
+    /* Read kernal info */
+    unsigned proc_count;
+    unsigned jobs_count;
+    fscanf(kinfo, "%u %u", &proc_count, &jobs_count);
+    if (proc_count >= PROC_MAX)
+    {
+        printf("too many processes!\n");
+        return;
+    }
+    printf("%u processes, %u jobs\n", proc_count, jobs_count);
+
+
     /* Read process info */
-    for (dir = readdir(proc), i = 0; dir != NULL && i < PAGE_MAX; dir = readdir(proc))
+    for (dir = readdir(proc), i = 0; dir != NULL && i < proc_count; dir = readdir(proc))
     {
         if (dir->d_type == DT_DIR && strcmp(dir->d_name, ".") && strcmp(dir->d_name, ".."))
         {
@@ -49,11 +75,13 @@ void top()
     {
         cpu_ticks += process[j].ticks;
     }
+
     /* Print head */
     printf("PID USERNAME PRI NICE    SIZE STATE   TIME     CPU COMMAND\n");
+    /* Print proc info */
     for (int j = 0; j < i; j++)
     {
-        printf("%3d %-8s %1d %4d %6luK %7c %4d   %2.2lf%% %s\n", process[j].pid, process[j].username, process[j].priority, process[j].nice, process[j].memory / 1000, process[j].state, process[j].ticks/60, process[j].ticks / (double)cpu_ticks, process[j].name);
+        printf("%3d %-8s %1d %4d %6luK %7c %3lu      %2.2lf%% %s\n", process[j].pid, process[j].username, process[j].priority, process[j].nice, process[j].memory / 1000, process[j].state, process[j].ticks/60, process[j].ticks / (double)cpu_ticks, process[j].name);
     }
 
 }
