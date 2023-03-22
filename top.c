a2d4fda4e8a1577a05dc9bbcee39e0ac8a06ca25
Wu Mianzhi <1427207878@qq.com>
2023-03-22 10:21:00 +0800

    format

--- top.c
+++ top.c
@@ -15,8 +15,7 @@ static void process_info(char* pid, struct proc * p)
 {
     FILE* fp;
     char buf[NAME_MAX + 1] = "";
-    sprintf(buf, "/proc/%s/psinfo", pid);
-    //printf("%s\n", buf);
+    sprintf(buf, "/proc/%s/psinfo", pid); // prepare psinfo file name
     fp = fopen(buf, "r");
     p->name[0] = '\0';
     p->username[0] = '\0';
@@ -32,28 +31,29 @@ void top()
     DIR * proc = opendir("/proc");
     struct dirent * dir;
     struct proc process[PAGE_MAX];
-    char buf[256] = "/proc/";
     unsigned long cpu_ticks = 0;
     int i = 0;
     printf("TOP START\n");
+    /* Read process info */
     for (dir = readdir(proc), i = 0; dir != NULL && i < PAGE_MAX; dir = readdir(proc))
     {
         if (dir->d_type == DT_DIR && strcmp(dir->d_name, ".") && strcmp(dir->d_name, ".."))
         {
             process[i].pid = strtol(dir->d_name, NULL, 10);
             process_info(dir->d_name, &process[i]);
-            //printf("%d\n", process[i].pid);
             i++;
-            //printf("%d %16s %d %luK %c %d %lf%% %s", atoi(dir->d_name), p.username, p.priority, p.memory / 1000, p.state, p.ticks/60, p.ticks);
         }
     }
+    /* Caculate cpu ticks in all */
     for (int j = 0; j < i; j++)
     {
         cpu_ticks += process[j].ticks;
     }
+    /* Print head */
+    printf("PID USERNAME PRI NICE    SIZE STATE   TIME     CPU COMMAND\n");
     for (int j = 0; j < i; j++)
     {
-        printf("%d %16s %d %luK %c %d %lf%% %s\n", process[j].pid, process[j].username, process[j].priority, process[j].memory / 1000, process[j].state, process[j].ticks/60, process[j].ticks / (double) cpu_ticks, process[j].name);
+        printf("%3d %-8s %1d %4d %6luK %7c %4d   %2.2lf%% %s\n", process[j].pid, process[j].username, process[j].priority, process[j].nice, process[j].memory / 1000, process[j].state, process[j].ticks/(double)60, process[j].ticks / (double) cpu_ticks, process[j].name);
     }
 
 }
